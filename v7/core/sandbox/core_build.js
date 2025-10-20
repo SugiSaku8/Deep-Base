415c2b62d2f605260259d1ea83e7e2e959041949
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2025-10-20 11:19:36 +0900

    Update core_build.js

--- v7/core/sandbox/core_build.js
+++ v7/core/sandbox/core_build.js
@@ -1,2 +1,15 @@
-const core_build = {};
+const core_build = class{
+  constructor(){
+    
+  }
+  generate(type){
+    switch(type){
+      case "basic_config":
+        //write here code of generate basic_config
+      default:
+        console.log(`The option you specified ${type} is not registered as a build option.
+Please check db.sandbox.build.help().`);
+    }
+  }
+};
 export default core_build;
