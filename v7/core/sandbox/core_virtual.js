11828cceaddc389d097e128adf73fbb643c8af4c
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2025-10-20 12:35:17 +0900

    Update core_virtual.js

--- v7/core/sandbox/core_virtual.js
+++ v7/core/sandbox/core_virtual.js
@@ -1,2 +1,21 @@
-const core_virtual = {};
+const core_virtual = class{
+  contructor(callback,v_default) {
+  const iframe = document.getElementById('sandbox');
+  if (!iframe) {
+  
+  }
+
+  // iframeが完全にロードされた後にコードを実行
+  iframe.onload = function () {
+    const iframeWindow = iframe.contentWindow;
+    iframeWindow.safeGlobalVar = v_default;
+
+    callback(iframeWindow);
+  };
+
+  // 環境をクリアするためにiframeをリロード
+  iframe.src = 'about:blank';
+}
+
+};
 export default core_virtual;
